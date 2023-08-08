4122bed0830b7b8c69e578c6fe3e6166a2efc7a3
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-08-09 02:52:33 +0530

    Higher Order Fns

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
+// Higher Order Fns: takes fn as input or returns it or both
+
+function greet(fn) {
+  console.log(fn());
+}
+
 function sayHello() {
-  console.log("Hello World!");
-}
-let fn = sayHello; //assigned to a var. passing reference.
-
-function greet(fnMessage) {
-  // passed as arg
-  console.log(fnMessage());
-}
-greet(sayHello);
-
-function retrunMessageFn() {
   // returning fn
   return function () {
-    console.log("I am returning a fn");
+    return "I am returning a fn";
   };
 }
-let messageFn = retrunMessageFn();
-let message = messageFn();
