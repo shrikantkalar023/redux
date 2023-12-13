57ce96e04384ea084e11e9f7b4940a099fa515c6
Shrikant Kalar <shrikantkalar023@gmail.com>
2023-12-13 18:30:07 +0530

    added vite react to proj

--- src/store/configureStore.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import reducer from "./bugs";
-import { devToolsEnhancer } from "@redux-devtools/extension";
-
-const configureStore = () =>
-  createStore(reducer, devToolsEnhancer({ trace: true }));
-
-export default configureStore;
